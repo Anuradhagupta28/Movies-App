dca1bbe885773af0e50acec98cb9dd66982ff2e5
Anuradhagupta28 <annugupta8512@gmail.com>
2023-06-04 13:37:23 +0530

    movies

--- movies-app/src/Components/Home.jsx
+++ movies-app/src/Components/Home.jsx
@@ -7,8 +7,8 @@ function Home() {
             <Center  w='100%'  bg='red.300'><Text fontWeight="bold"  fontSize='2xl'>Movies App</Text></Center> 
         
            <Flex  h='100%' w='100%'>
-        <Center  w='50%'  bg='green.500'><Text fontWeight="bold"  fontSize='2xl'>Admin Section</Text></Center>
-        <Center   w='50%' bg='blue.500'><Text fontWeight="bold" fontSize='2xl'>User Section</Text></Center>
+        <Center  w='50%'  bg='green.500'><Link to='/admin'><Text fontWeight="bold"  fontSize='2xl'>Admin Section</Text></Link></Center>
+        <Center   w='50%' bg='blue.500'><Link to='/user'><Text fontWeight="bold" fontSize='2xl'>User Section</Text></Link></Center>
       </Flex>
         
      
